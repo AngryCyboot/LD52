d6fad45f86c075a5e21ffd1475de5a1515120073
Florian Delrieu <florian.delrieu@free.fr>
2023-01-07 13:46:25 +0100

    Ajout script survivant

---
+++ GodotFiles/scripts/Survivant.gd
@@ -0,0 +1,17 @@
+extends Mesh
+
+var Botanist = int()
+var Engineering = int()
+var Chemistry = int()
+
+var Class = "Survivant"
+
+var Class_list = [
+				"Survivant",  # Sans spé
+				"Ingénieur",
+				"Botaniste",
+				"Chimiste"
+				]
+
+func _ready():
+	pass # Replace with function body.
