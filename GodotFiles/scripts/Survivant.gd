3ae24c309e670a4e0c474c0600f55a9db51c03ab
AngryCyboot <cyboot25@gmail.com>
2023-01-08 02:10:29 +0100

    survivant folow click ... kinda

--- GodotFiles/scripts/Survivant.gd
+++ GodotFiles/scripts/Survivant.gd
@@ -1,5 +1,10 @@
 extends Spatial
 
+var camera
+var isSelected = false
+var from
+var to
+
 var Botanist = int()
 var Engineering = int()
 var Chemistry = int()
@@ -14,4 +19,21 @@ var Class_list = [
 				]
 
 func _ready():
+	camera = $"../Camera"
 	pass # Replace with function body.
+
+func _input(event):
+	if event is InputEventMouseButton and event.is_pressed():
+		isSelected = true
+
+func _physics_process(delta):
+	if isSelected == true :
+		var mouse_pos = get_viewport().get_mouse_position()
+		from = camera.project_ray_origin(mouse_pos)
+		to = from + camera.project_ray_normal(mouse_pos) * 10000
+		var space_state = get_world().get_direct_space_state()
+		var result = space_state.intersect_ray( from, to)
+		if result:
+			self.transform.origin = result.position
+		isSelected = false
+	pass
