1d5be77293463c23d3fdfd9692f8d531a20fc80b
LoneWolf <delrieu.adrien@gmail.com>
2023-01-08 01:55:34 +0100

    Add getter status

--- GodotFiles/scripts/Batiment.gd
+++ GodotFiles/scripts/Batiment.gd
@@ -2,7 +2,8 @@ extends MeshInstance
 export (Material) var damageMaterial = preload("res://materials et shaders/Batiment/Damage.tres")
 export (Material) var activeMaterial = preload("res://materials et shaders/Batiment/Active.tres")
 export (Material) var idleMaterial = preload("res://materials et shaders/Batiment/Idle.tres")
- 
+var status = "active"
+
 func _input(event):
 	if event is InputEventKey and event.pressed:
 		if event.scancode == KEY_D:
@@ -14,16 +15,26 @@ func _input(event):
 
 func set_damage(): # Asign damage (red color) to building
 	self.get_mesh().surface_set_material(0,damageMaterial)
+	status = "damage"
 
 func set_actif(): # Asign active (green color) to building
 	self.get_mesh().surface_set_material(0,activeMaterial)
+	status = "active"
 
 func set_inactif(): # Asign idle (yellow color) to building
+	
 	self.get_mesh().surface_set_material(0,idleMaterial)
+	print("change")
+	status = "idle"
+	
+func status():
+	return status
+	
 
 
 func _ready():
-	pass 
+	print("loaded")
+	set_inactif()
 
 #func _process(delta):
 #	pass
