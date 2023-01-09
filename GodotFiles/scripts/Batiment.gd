0d4acc5ae8a4ea8c6ffcd2c548c7b59ae3085202
AngryCyboot <cyboot25@gmail.com>
2023-01-09 01:33:50 +0100

    improved activable building

--- GodotFiles/scripts/Batiment.gd
+++ GodotFiles/scripts/Batiment.gd
@@ -1,8 +1,12 @@
 extends MeshInstance
+
 export (Material) var damageMaterial = preload("res://materials et shaders/Batiment/Damage.tres")
 export (Material) var activeMaterial = preload("res://materials et shaders/Batiment/Active.tres")
 export (Material) var idleMaterial = preload("res://materials et shaders/Batiment/Idle.tres")
-var status = "active"
+
+var active = false
+var damage = false
+var survivors = 0
 
 func _input(event):
 	if event is InputEventKey and event.pressed:
@@ -12,27 +16,41 @@ func _input(event):
 			set_actif()
 		if event.scancode == KEY_I:
 			set_inactif()
+	
+func body_entered(plop):
+	survivors += 1
+	pass
 
+func body_exited(plop):
+	survivors -= 1
+	print(survivors)
+	pass
+	
 func set_damage(): # Asign damage (red color) to building
 	self.get_mesh().surface_set_material(0,damageMaterial)
-	status = "damage"
+	#status = "damage"
 
 func set_actif(): # Asign active (green color) to building
 	self.get_mesh().surface_set_material(0,activeMaterial)
-	status = "active"
+	active = true
+	$SpotLight.visible = active
+	#status = "active"
 
 func set_inactif(): # Asign idle (yellow color) to building
-	
 	self.get_mesh().surface_set_material(0,idleMaterial)
-	status = "idle"
+	active = false
+	$SpotLight.visible = active
+	#status = "idle"
 	
-func status():
-	return status
+#func status():
+	#return status
 	
-
-
 func _ready():
 	set_inactif()
 
-#func _process(delta):
-#	pass
+func _process(delta):
+	if survivors > 0 :
+		set_actif()
+	else:
+		set_inactif()
+	pass
