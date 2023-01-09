3578e97494a596fcd062d4beb19062981dbca0ed
AngryCyboot <cyboot25@gmail.com>
2023-01-09 18:35:06 +0100

    settings for buildings

--- GodotFiles/scripts/Batiment.gd
+++ GodotFiles/scripts/Batiment.gd
@@ -1,7 +1,7 @@
 extends MeshInstance
 
-export (Material) var damageMaterial = preload("res://materials et shaders/Batiment/Damage.tres")
-export (Material) var activeMaterial = preload("res://materials et shaders/Batiment/Active.tres")
+export (Material) var damageMaterial #= preload("res://materials et shaders/Batiment/Damage.tres")
+export (Material) var activeMaterial #= preload("res://materials et shaders/Batiment/Active.tres")
 #export (Material) var idleMaterial = preload("res://materials et shaders/Batiment/Idle.tres")
 
 export (int) var Food = 0
@@ -14,16 +14,18 @@ var prod = Array()
 
 var active = false
 export var damage = false
+export var needSurvivorDefault = true
 var survivors = 0
+var needSurvivor
 
-func _input(event):
-	if event is InputEventKey and event.pressed:
-		if event.scancode == KEY_D:
-			set_damage()
-		if event.scancode == KEY_A:
-			set_actif()
-		if event.scancode == KEY_I:
-			set_inactif()
+#func _input(event):
+	#if event is InputEventKey and event.pressed:
+		#if event.scancode == KEY_D:
+			#set_damage()
+		#if event.scancode == KEY_A:
+			#set_actif()
+		#if event.scancode == KEY_I:
+			#set_inactif()
 	
 func body_entered(plop):
 	survivors += 1
@@ -35,9 +37,9 @@ func body_exited(plop):
 	
 func set_damage(): # Asign damage (red color) to building
 	if damage:
-		self.get_mesh().surface_set_material(0,damageMaterial)
+		self.set_surface_material(0,damageMaterial)
 	else:
-		self.get_mesh().surface_set_material(0,activeMaterial)
+		self.set_surface_material(0,activeMaterial)
 	
 	#status = "damage"
 
@@ -58,10 +60,21 @@ func set_inactif(): # Asign idle (yellow color) to building
 	
 func _ready():
 	set_inactif()
+	set_damage()
+	needSurvivor = needSurvivorDefault
 	prod = [Energy,Oxy,Shit,Water,Food,active,damage]
-	self.get_mesh().surface_set_material(0,activeMaterial)
 
 func _process(delta):
+	if damage:
+		needSurvivor = true
+	else:
+		needSurvivor = needSurvivorDefault
+	if needSurvivor:
+		$Placement.visible = true
+		$Area.monitoring = true
+	else:
+		$Placement.visible = false
+		$Area.monitoring = false
 	if survivors > 0 :
 		set_actif()
 	else:
